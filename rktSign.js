394075d02c7527199402ae4ec7588d5deac5a7c4
Sky Dev <37593290+skyrocket-dev@users.noreply.github.com>
2023-10-05 09:11:31 -0700

    Create rktSign.js

---
+++ rktSign.js
@@ -0,0 +1,36 @@
+let InitializeSignature = () => {
+  var userAgent = navigator.userAgent.toLowerCase();
+  var supported = /(chrome|firefox)/;
+  if (supported.test(userAgent.toLowerCase())) {
+    var dark = [
+      'background-color: #171718',
+      'color: #ffffff',
+      'padding: 20px 16px 16px 16px',
+      'font-size: 16px;'
+    ].join(';');
+  
+    var blue = [
+      'padding: 20px 5px 16px',
+      'background-color: #1727ff',
+      'color: #ffffff',
+      'font-size: 16px;'
+    ].join(';');
+  
+    var spacer = ['background-color: transparent'].join(';');
+  
+    var msg =
+      '\n\n %c Crafted by Skyrocket Digital %c https://skyrocket.is %c \n\n\n';
+  
+    console.log(msg, dark, blue, spacer);
+  } else if (window.console) {
+    console.log('Crafted by Skyrocket Digital https://skyrocket.is ');
+  }
+}
+
+if (/complete|interactive|loaded/.test(document.readyState)) {
+    InitializeSignature();
+} else { 
+    window.addEventListener('DOMContentLoaded', function () { 
+        InitializeSignature();
+    })
+}
